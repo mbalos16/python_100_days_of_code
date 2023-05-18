fbb5d5f2b989587a47a95d1c35c0b6744c438817
Maria Balos <mariabalos16@gmail.com>
2023-05-18 13:24:13 +0100

    Add title, redefine function - Day 8 Caesar ciper

--- day_08/exercise8_3_caesar_cipher.py
+++ day_08/exercise8_3_caesar_cipher.py
@@ -1,3 +1,5 @@
+# Exercise 8.3 - Caesar_cipher
+
 alphabet = [
     "a",
     "b",
@@ -31,43 +33,40 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    encripted_message = []
-    # Taking each letter in the provided text and checking it with the position of the same letter in the alphabet
-    for letter in plain_text:
-        letter_index = alphabet.index(letter)
-        new_position = letter_index + shift_amount
-        
-        # Checking if the length of the new position is inferior to the length of the alphabet list plus shift. If so, the new_position is part of the main length of the list.
-        if new_position < (len(alphabet) - shift_amount):
-            new_letter = alphabet[new_position]
-            encripted_message.append(new_letter)
 
-        # Checking if the length of the new_position is superior or equal to the length of the alphabet list plus shift. If so, the new_position is part of the main length of the list.
-        if new_position >= (len(alphabet) - shift_amount):
-            new_letter_2 = alphabet[new_position - len(alphabet)]
-            encripted_message.append(new_letter_2)
-    print(f"The encoded text is: {str(encripted_message)}.")
+def caesar(shift_paramether, text_parameter, direction_parameter):
+    if direction_parameter == "encode":
+        encripted_message = []
+        # Taking each letter in the provided text and checking it with the position of the same letter in the alphabet
+        for letter in text_parameter:
+            letter_index = alphabet.index(letter)
+            new_position = letter_index + shift_paramether
+
+            # Checking if the length of the new position is inferior to the length of the alphabet list plus shift. If so, the new_position is part of the main length of the list.
+            if new_position < (len(alphabet) - shift_paramether):
+                new_letter = alphabet[new_position]
+                encripted_message.append(new_letter)
+
+            # Checking if the length of the new_position is superior or equal to the length of the alphabet list plus shift. If so, the new_position is part of the main length of the list.
+            if new_position >= (len(alphabet) - shift_paramether):
+                new_letter_2 = alphabet[new_position - len(alphabet)]
+                encripted_message.append(new_letter_2)
+        print(f"The encoded text_parameter is: {str(encripted_message)}.")
+
+    if direction_parameter == "decode":
+        decoded_message = []
+        # Taking each letter in the provided encripted message and checking it with the position of the same letter in the alphabet
+        for letter in text_parameter:
+            letter_index = alphabet.index(letter)
+            new_position = letter_index - shift_paramether
+
+            if new_position < (len(alphabet) - shift_paramether):
+                new_letter = alphabet[new_position]
+                decoded_message.append(new_letter)
+            if new_position >= (len(alphabet) - shift_paramether):
+                new_letter_2 = alphabet[new_position - len(alphabet)]
+                decoded_message.append(new_letter_2)
+        print(f"The decoded text_parameter is: {str(decoded_message)}.")
 
 
-def decrypt(encripted_message, shift_amount):
-    decoded_message = []
-    # Taking each letter in the provided encripted message and checking it with the position of the same letter in the alphabet
-    for letter in encripted_message:
-        letter_index = alphabet.index(letter)
-        new_position = letter_index - shift_amount
-        
-        if new_position < (len(alphabet) - shift_amount):
-            new_letter = alphabet[new_position]
-            decoded_message.append(new_letter)
-            
-        if new_position >= (len(alphabet) - shift_amount):
-            new_letter_2 = alphabet[new_position - len(alphabet)]
-            decoded_message.append(new_letter_2)
-    print(f"The decoded text is: {str(decoded_message)}.")
-
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-else:
-    decrypt(encripted_message=text, shift_amount=shift)
+caesar(shift_paramether=shift, direction_parameter=direction, text_parameter=text)
