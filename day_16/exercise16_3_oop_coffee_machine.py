c164b43f86a5e78845f9155b43db44391c00ea34
Maria Balos <mariabalos16@gmail.com>
2023-06-01 17:21:54 +0100

    Solve report problem - Day 16

--- day_16/exercise16_3_oop_coffee_machine.py
+++ day_16/exercise16_3_oop_coffee_machine.py
@@ -5,16 +5,14 @@ from exercise16_3_oop_coffee_machine_coffee_maker import CoffeeMaker
 from exercise16_3_oop_coffee_machine_money_machine import MoneyMachine
 
 machine_working = True
+menu_available = Menu()
+new_coffee = CoffeeMaker()
+money = MoneyMachine()
+
 while machine_working:
-    menu_available = Menu()
     availability = menu_available.get_items()
-
     order = input(f"What would you like?({availability}): ").lower()
-
-    new_coffee = CoffeeMaker()
-    money = MoneyMachine()
     if order == "off":
-        print("Sorry 😓, this function is not available.")
         machine_working = False
     elif order == "report":
         new_coffee.report()
@@ -23,12 +21,6 @@ while machine_working:
         enough_resources = new_coffee.is_resource_sufficient(
             menu_available.find_drink(order)
         )
-        if enough_resources:
-            payment = money.make_payment(menu_available.find_drink(order).cost)
-            if payment:
-                new_coffee.make_coffee(menu_available.find_drink(order))
-            else:
-                print("Sorry that's not enough money. Money refunded.")
-        else:
-            print("Sorry there is not enough resources for this order.")
-            machine_working = False
+        payment = money.make_payment(menu_available.find_drink(order).cost)
+        if enough_resources and payment:
+            new_coffee.make_coffee(menu_available.find_drink(order))
