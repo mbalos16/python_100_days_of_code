43b0cd7ca636b0fcf3b16929ac9da43959e00d28
Maria Balos <mariabalos16@gmail.com>
2023-07-18 14:13:23 +0100

    Day 32 - Formatting

--- day_33/exercise_33_2_iss_in_the_sky.py
+++ day_33/exercise_33_2_iss_in_the_sky.py
@@ -1,13 +1,13 @@
 import requests
 from datetime import datetime
 import smtplib
+import time
 
 MY_LAT = ["insert your latitude here"]
 MY_LON = ["insert your longitude here"]
 FROM_EMAIL = "socmariabalos@gmail.com"
 PERSONAL_API_PASSWORD = "[insert your password here]"
 TO_EMAIL = "socmariabalos@gmail.com"
-import time
 
 
 # If the ISS is close to my current possition
@@ -16,12 +16,16 @@ def check_position():
     response = requests.get("http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
-    
+
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
-    
-    if MY_LAT - 5 <= iss_latitude >= MY_LAT + 5 and MY_LON - 5 <= iss_longitude <= MY_LON + 5:
+
+    if (
+        MY_LAT - 5 <= iss_latitude >= MY_LAT + 5
+        and MY_LON - 5 <= iss_longitude <= MY_LON + 5
+    ):
         return True
+
 
 # 1. and it is currently dark
 def check_night():
@@ -31,20 +35,22 @@ def check_night():
         "lng": MY_LON,
         "formatted": 0,
     }
-    
-    response = requests.get(url="https://api.sunrise-sunset.org/json",
-                            params=parameters)
+
+    response = requests.get(
+        url="https://api.sunrise-sunset.org/json", params=parameters
+    )
     response.raise_for_status()
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-    
+
     time_now = datetime.now()
 
     if time_now.hour < sunrise and time_now.hour > sunset:
         return True
     else:
         return False
+
 
 def send_email():
     my_email = FROM_EMAIL
@@ -60,6 +66,7 @@ def send_email():
             to_addrs=destination,
             msg=msg.encode("utf-8"),
         )
+
 
 while True:
     time.sleep(60)
