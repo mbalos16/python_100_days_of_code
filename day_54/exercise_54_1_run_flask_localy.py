2c77b1b13827b359a156a159ce3063a23fff906d
Maria Balos <mariabalos16@gmail.com>
2023-08-03 17:48:28 +0100

    Add day 54 - Python decorators & Flask

---
+++ day_54/exercise_54_1_run_flask_localy.py
@@ -0,0 +1,13 @@
+# Exercise 54.1. Run flask localy
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ day_54/exercise_54_2_function_time_python_decorator.py
@@ -0,0 +1,32 @@
+# Exercise 54.2.functiontime python decorator
+# Given the above information, complete the code exercise by printing out the speed it takes to run the fast_function() vs the slow_function(). You will need to complete the speed_calc_decorator() function.
+import time
+
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(function):
+    def time_diference():
+        beggining_time = time.time()
+        function
+        end_time = time.time()
+        print(f"Time: {end_time - beggining_time}")
+
+    return time_diference
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+
+fast_function()
+slow_function()
