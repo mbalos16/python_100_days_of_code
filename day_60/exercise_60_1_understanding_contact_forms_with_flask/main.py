424b2da8768b02aef2955a372f2e3fca49f9ffd0
Maria Balos <mariabalos16@gmail.com>
2024-02-22 17:54:48 +0000

    Day 60 - Fix brackets error

--- day_60/exercise_60_1_understanding_contact_forms_with_flask/main.py
+++ day_60/exercise_60_1_understanding_contact_forms_with_flask/main.py
@@ -12,7 +12,7 @@ def home():
 def receive_data():
     name = request.form["username"]
     password = request.form["password"]
-    return f"<h1>Name: {{name}}, Password: {{password}}</h1>"
+    return f"<h1>Name: {name}, Password: {password}</h1>"
 
 
 if __name__ == "__main__":
